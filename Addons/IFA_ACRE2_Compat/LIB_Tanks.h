42fa13c85cd6e6bfe3f0a4ec21a91b29a47bb60c
Drofseh <Drofseh@users.noreply.github.com>
2017-05-30 21:46:01 -0700

    Add files via upload

---
+++ Addons/IFA_ACRE2_Compat/LIB_Tanks.h
@@ -0,0 +1,147 @@
+	// German tanks
+	// Panzer IV
+ class LIB_PzKpfwIV_H_base : LIB_Tank_base {
+
+  // Crew Intercom
+  acre_hasCrewIntercom = 1;
+  acre_crewIntercomPositions[] = {"crew"};
+
+  // No Passenger intercom
+
+  // Infantry Phone
+  acre_hasInfantryPhone = 1;
+  acre_infantryPhoneIntercom[] = {"crew"};
+  acre_infantryPhonePosition[] = {-0.45, -2.8, -1.1};
+  //acre_infantryPhoneCustomRinging[] = {"\IFA_ACRE2_Compat\Addons\IFA_ACRE2_Compat\Sounds\InFoneGer.ogg", 7.0, 1.0, 1.0, 50};
+
+  class AcreRacks {
+   class Rack_1 {
+    name = "Tank Radio";
+    componentname = "ACRE_VRC111";
+    allowed[] = {"commander", {"gunner", 1}};
+    disabled[] = {};
+    defaultComponents[] = {};
+    mountedRadio = "ACRE_PRC148";
+    isRadioRemovable = 0;
+    intercom[] = {"crew"};
+   };
+  };
+ };
+
+	// Panther
+ class LIB_PzKpfwV_base : LIB_Tank_base {
+
+  // Crew Intercom
+  acre_hasCrewIntercom = 1;
+  acre_crewIntercomPositions[] = {"crew"};
+
+  // No Passenger intercom
+
+  // Infantry Phone
+  acre_hasInfantryPhone = 1;
+  acre_infantryPhoneIntercom[] = {"crew"};
+  acre_infantryPhonePosition[] = {-0.45, -2.8, -1.1};
+  //acre_infantryPhoneCustomRinging[] = {"\IFA_ACRE2_Compat\Addons\IFA_ACRE2_Compat\Sounds\InFoneGer.ogg", 7.0, 1.0, 1.0, 50};
+
+  class AcreRacks {
+   class Rack_1 {
+    name = "Tank Radio";
+    componentname = "ACRE_VRC111";
+    allowed[] = {"commander", {"gunner", 1}};
+    disabled[] = {};
+    defaultComponents[] = {};
+    mountedRadio = "ACRE_PRC148";
+    isRadioRemovable = 0;
+    intercom[] = {"crew"};
+   };
+  };
+ };
+
+	// Kingtiger
+ class LIB_PzKpfwVI_B_base : LIB_Tank_base {
+
+  // Crew Intercom
+  acre_hasCrewIntercom = 1;
+  acre_crewIntercomPositions[] = {"crew"};
+
+  // No Passenger intercom
+
+  // Infantry Phone
+  acre_hasInfantryPhone = 1;
+  acre_infantryPhoneIntercom[] = {"crew"};
+  acre_infantryPhonePosition[] = {-0.45, -2.8, -1.1};
+  //acre_infantryPhoneCustomRinging[] = {"\IFA_ACRE2_Compat\Addons\IFA_ACRE2_Compat\Sounds\InFoneGer.ogg", 7.0, 1.0, 1.0, 50};
+
+  class AcreRacks {
+   class Rack_1 {
+    name = "Tank Radio";
+    componentname = "ACRE_VRC111";
+    allowed[] = {"commander", {"gunner", 1}};
+    disabled[] = {};
+    defaultComponents[] = {};
+    mountedRadio = "ACRE_PRC148";
+    isRadioRemovable = 0;
+    intercom[] = {"crew"};
+   };
+  };
+ };
+
+	// Tiger I
+ class LIB_PzKpfwVI_E_base : LIB_Tank_base {
+
+  // Crew Intercom
+  acre_hasCrewIntercom = 1;
+  acre_crewIntercomPositions[] = {"crew"};
+
+  // No Passenger intercom
+
+  // Infantry Phone
+  acre_hasInfantryPhone = 1;
+  acre_infantryPhoneIntercom[] = {"crew"};
+  acre_infantryPhonePosition[] = {-0.45, -2.8, -1.1};
+  //acre_infantryPhoneCustomRinging[] = {"\IFA_ACRE2_Compat\Addons\IFA_ACRE2_Compat\Sounds\InFoneGer.ogg", 7.0, 1.0, 1.0, 50};
+
+  class AcreRacks {
+   class Rack_1 {
+    name = "Tank Radio";
+    componentname = "ACRE_VRC111";
+    allowed[] = {"commander", {"gunner", 1}};
+    disabled[] = {};
+    defaultComponents[] = {};
+    mountedRadio = "ACRE_PRC148";
+    isRadioRemovable = 0;
+    intercom[] = {"crew"};
+   };
+  };
+ };
+
+	// StuG III
+ class LIB_StuG_III_G_base : LIB_Tank_base {
+
+  // Crew Intercom
+  acre_hasCrewIntercom = 1;
+  acre_crewIntercomPositions[] = {"crew"};
+
+  // No Passenger intercom
+
+  // Infantry Phone
+  acre_hasInfantryPhone = 1;
+  acre_infantryPhoneIntercom[] = {"crew"};
+  acre_infantryPhonePosition[] = {-0.45, -2.8, -1.1};
+  //acre_infantryPhoneCustomRinging[] = {"\IFA_ACRE2_Compat\Addons\IFA_ACRE2_Compat\Sounds\InFoneGer.ogg", 7.0, 1.0, 1.0, 50};
+
+  class AcreRacks {
+   class Rack_1 {
+    name = "Tank Radio";
+    componentname = "ACRE_VRC111";
+    allowed[] = {"commander"};
+    disabled[] = {};
+    defaultComponents[] = {};
+    mountedRadio = "ACRE_PRC148";
+    isRadioRemovable = 0;
+    intercom[] = {"crew"};
+   };
+  };
+ };
+
+	//Soviet Tanks
