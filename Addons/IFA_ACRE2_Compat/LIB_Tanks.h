0c627b5c82d7eb2aee91d92bf619d51743581e5e
Drofseh <Drofseh@users.noreply.github.com>
2017-06-01 18:20:37 -0700

    Configure German vehicle intercoms, infantry phones, and radio racks
    Configure German vehicle intercoms, infantry phones, and radio racks

--- Addons/IFA_ACRE2_Compat/LIB_Tanks.h
+++ Addons/IFA_ACRE2_Compat/LIB_Tanks.h
@@ -1,134 +1,116 @@
-	// German tanks
-	// Panzer IV
+    // German tanks
+    // Panzer IV
  class LIB_PzKpfwIV_H_base : LIB_Tank_base {
 
-  // Crew Intercom
-  acre_hasCrewIntercom = 1;
-  acre_crewIntercomPositions[] = {"crew"};
-
-  // No Passenger intercom
-
   // Infantry Phone
-  acre_hasInfantryPhone = 1;
-  acre_infantryPhoneIntercom[] = {"crew"};
   acre_infantryPhonePosition[] = {-0.45, -2.8, -1.1};
-  //acre_infantryPhoneCustomRinging[] = {"\IFA_ACRE2_Compat\Addons\IFA_ACRE2_Compat\Sounds\InFoneGer.ogg", 7.0, 1.0, 1.0, 50};
-
-  class AcreRacks {
-   class Rack_1 {
-    name = "Tank Radio";
-    componentname = "ACRE_VRC111";
-    allowed[] = {"commander", {"gunner", 1}};
-    disabled[] = {};
-    defaultComponents[] = {};
-    mountedRadio = "ACRE_PRC148";
-    isRadioRemovable = 0;
-    intercom[] = {"crew"};
-   };
-  };
+  //acre_infantryPhoneCustomRinging[] = {"InFoneGer.wav", 7.0, 1.0, 1.0, 50};
  };
 
-	// Panther
+    // Panther
  class LIB_PzKpfwV_base : LIB_Tank_base {
 
-  // Crew Intercom
-  acre_hasCrewIntercom = 1;
-  acre_crewIntercomPositions[] = {"crew"};
-
-  // No Passenger intercom
-
   // Infantry Phone
-  acre_hasInfantryPhone = 1;
-  acre_infantryPhoneIntercom[] = {"crew"};
-  acre_infantryPhonePosition[] = {-0.45, -2.8, -1.1};
-  //acre_infantryPhoneCustomRinging[] = {"\IFA_ACRE2_Compat\Addons\IFA_ACRE2_Compat\Sounds\InFoneGer.ogg", 7.0, 1.0, 1.0, 50};
-
-  class AcreRacks {
-   class Rack_1 {
-    name = "Tank Radio";
-    componentname = "ACRE_VRC111";
-    allowed[] = {"commander", {"gunner", 1}};
-    disabled[] = {};
-    defaultComponents[] = {};
-    mountedRadio = "ACRE_PRC148";
-    isRadioRemovable = 0;
-    intercom[] = {"crew"};
-   };
-  };
+  acre_infantryPhonePosition[] = {-1.0, -3.6, -0.9};
+  //acre_infantryPhoneCustomRinging[] = {"InFoneGer.wav", 7.0, 1.0, 1.0, 50};
  };
 
-	// Kingtiger
+    // Kingtiger
  class LIB_PzKpfwVI_B_base : LIB_Tank_base {
 
-  // Crew Intercom
-  acre_hasCrewIntercom = 1;
-  acre_crewIntercomPositions[] = {"crew"};
-
-  // No Passenger intercom
-
   // Infantry Phone
-  acre_hasInfantryPhone = 1;
-  acre_infantryPhoneIntercom[] = {"crew"};
-  acre_infantryPhonePosition[] = {-0.45, -2.8, -1.1};
-  //acre_infantryPhoneCustomRinging[] = {"\IFA_ACRE2_Compat\Addons\IFA_ACRE2_Compat\Sounds\InFoneGer.ogg", 7.0, 1.0, 1.0, 50};
-
-  class AcreRacks {
-   class Rack_1 {
-    name = "Tank Radio";
-    componentname = "ACRE_VRC111";
-    allowed[] = {"commander", {"gunner", 1}};
-    disabled[] = {};
-    defaultComponents[] = {};
-    mountedRadio = "ACRE_PRC148";
-    isRadioRemovable = 0;
-    intercom[] = {"crew"};
-   };
-  };
+  acre_infantryPhonePosition[] = {1.025, -3.9, -0.8};
+  //acre_infantryPhoneCustomRinging[] = {"InFoneGer.wav", 7.0, 1.0, 1.0, 50};
  };
 
-	// Tiger I
+    // Tiger I
  class LIB_PzKpfwVI_E_base : LIB_Tank_base {
 
-  // Crew Intercom
-  acre_hasCrewIntercom = 1;
-  acre_crewIntercomPositions[] = {"crew"};
+  // Infantry Phone
+  acre_infantryPhonePosition[] = {0, -3.75, -1.1};
+  //acre_infantryPhoneCustomRinging[] = {"InFoneGer.wav", 7.0, 1.0, 1.0, 50};
+ };
 
-  // No Passenger intercom
+    // StuG III
+ class LIB_StuG_III_G_base : LIB_Tank_base {
 
   // Infantry Phone
-  acre_hasInfantryPhone = 1;
-  acre_infantryPhoneIntercom[] = {"crew"};
-  acre_infantryPhonePosition[] = {-0.45, -2.8, -1.1};
-  //acre_infantryPhoneCustomRinging[] = {"\IFA_ACRE2_Compat\Addons\IFA_ACRE2_Compat\Sounds\InFoneGer.ogg", 7.0, 1.0, 1.0, 50};
+  acre_infantryPhonePosition[] = {0, -2.8, -0.4};
+  //acre_infantryPhoneCustomRinging[] = {"InFoneGer.wav", 7.0, 1.0, 1.0, 50};
 
   class AcreRacks {
    class Rack_1 {
     name = "Tank Radio";
     componentname = "ACRE_VRC111";
-    allowed[] = {"commander", {"gunner", 1}};
-    disabled[] = {};
+    allowed[] = {"crew", "commander", {"turret", "all"}};
+    disabled[] = {"driver", "gunner"};
     defaultComponents[] = {};
     mountedRadio = "ACRE_PRC148";
     isRadioRemovable = 0;
-    intercom[] = {"crew"};
+    intercom[] = {"crew", "driver", "commander"};
    };
   };
  };
 
-	// StuG III
- class LIB_StuG_III_G_base : LIB_Tank_base {
+    //Soviet Tanks
+    //IS-2-43
+ class LIB_JS2_43_base : LIB_Tank_base {
+
+  // Infantry Phone
+  acre_infantryPhonePosition[] = {0, -3.9, -0.8};
+  //acre_infantryPhoneCustomRinging[] = {"InFoneRus.wav", 7.0, 1.0, 1.0, 50};
+ };
+
+    //Su-85
+ class LIB_SU85_base : LIB_Tank_base {
+     
+  // Infantry Phone
+  acre_infantryPhonePosition[] = {0, -3.2, -0.8};
+  //acre_infantryPhoneCustomRinging[] = {"InFoneRus.wav", 7.0, 1.0, 1.0, 50};
+ };
+
+    //T-34-76
+ class LIB_T34_76_base : LIB_Tank_base {
 
   // Crew Intercom
   acre_hasCrewIntercom = 1;
-  acre_crewIntercomPositions[] = {"crew"};
-
-  // No Passenger intercom
+  acre_crewIntercomPositions[] = {"crew", "driver"};
 
   // Infantry Phone
   acre_hasInfantryPhone = 1;
-  acre_infantryPhoneIntercom[] = {"crew"};
-  acre_infantryPhonePosition[] = {-0.45, -2.8, -1.1};
-  //acre_infantryPhoneCustomRinging[] = {"\IFA_ACRE2_Compat\Addons\IFA_ACRE2_Compat\Sounds\InFoneGer.ogg", 7.0, 1.0, 1.0, 50};
+  acre_infantryPhoneIntercom[] = {"crew", "driver"};
+  acre_infantryPhonePosition[] = {0, -2.4, -0.8};
+  //acre_infantryPhoneCustomRinging[] = {"InFoneRus.wav", 7.0, 1.0, 1.0, 50};
+
+  class AcreRacks {
+   class Rack_1 {
+    name = "Tank Radio";
+    componentname = "ACRE_VRC111";
+    allowed[] = {"crew", "gunner", {"turret", "all"}};
+    disabled[] = {"driver"};
+    defaultComponents[] = {};
+    mountedRadio = "ACRE_PRC148";
+    isRadioRemovable = 0;
+    intercom[] = {"crew", "driver", "gunner"};
+   };
+  };
+ };
+
+    //T-34-85
+ class LIB_T34_85_base : LIB_Tank_base {
+
+  // Infantry Phone
+  acre_infantryPhonePosition[] = {0, -3.2, -0.8};
+  //acre_infantryPhoneCustomRinging[] = {"InFoneRus.wav", 7.0, 1.0, 1.0, 50};
+ };
+
+    //US Tanks
+    //M4 Sherman
+ class LIB_M4A3_75_base : LIB_Tank_base {
+     
+  // Infantry Phone
+  //acre_infantryPhonePosition[] = {0, -3.2, -0.8};
+  // The ACE interaction point that this is based off of is messed up, when that gets fixed in the IFA/ACE compat this should get updated
 
   class AcreRacks {
    class Rack_1 {
@@ -139,9 +121,7 @@
     defaultComponents[] = {};
     mountedRadio = "ACRE_PRC148";
     isRadioRemovable = 0;
-    intercom[] = {"crew"};
+    intercom[] = {"crew", "driver", "commander"};
    };
   };
  };
-
-	//Soviet Tanks
--- Addons/IFA_ACRE2_Compat/config.cpp
+++ Addons/IFA_ACRE2_Compat/config.cpp
@@ -9,14 +9,177 @@ class CfgPatches {
 
 class cfgVehicles {
 
- class LIB_Boat_base;
- class LIB_Car_base;
- class LIB_Halftrack_base;
- class LIB_Tank_base;
- class LIB_Truck_base;
- class LIB_WheeledTracked_APC_base;
- class LIB_GER_Plane_base;
+    //Boats
+ class Boat_F;
+ class LIB_Boat_base : Boat_F {
 
+  // No Crew Intercom
+  acre_hasCrewIntercom = 1;
+
+  // No Passenger intercom
+  acre_hasPassengerIntercom = 0;
+
+  // No Infantry Phone
+  acre_hasInfantryPhone = 0;
+ };
+
+    //Planes
+ class Plane_Base_F;
+ class LIB_Plane_base : Plane_Base_F {
+
+  // Crew Intercom
+  acre_hasCrewIntercom = 1;
+  acre_crewIntercomPositions[] = {"default"};
+
+  // No Passenger intercom
+  acre_hasPassengerIntercom = 0;
+
+  // Infantry Phone
+  acre_hasInfantryPhone = 1;
+  acre_infantryPhoneDisableRinging = 1;
+  acre_infantryPhoneIntercom[] = {"crew"};
+  acre_infantryPhonePosition[] = {0, 0, 0};
+
+  class AcreRacks {
+   class Rack_1 {
+    name = "Air Radio";
+    componentname = "ACRE_VRC111";
+    allowed[] = {"driver"};
+    disabled[] = {};
+    defaultComponents[] = {};
+    mountedRadio = "ACRE_PRC148";
+    isRadioRemovable = 0;
+    intercom[] = {"crew"};
+   };
+   class Rack_2 {
+    name = "Ground Radio";
+    componentname = "ACRE_VRC111";
+    allowed[] = {"driver"};
+    disabled[] = {};
+    defaultComponents[] = {};
+    mountedRadio = "ACRE_PRC148";
+    isRadioRemovable = 0;
+    intercom[] = {"crew"};
+   };
+  };
+ };
+
+ class LIB_GER_Plane_base;
+ class LIB_SU_Plane_base;
+
+    //Cars
+ class Car_F;
+ class LIB_Car_base : Car_F {
+
+  // No Crew Intercom
+  acre_hasCrewIntercom = 0;
+
+  // No Passenger intercom
+  acre_hasPassengerIntercom = 0;
+
+  // No Infantry Phone
+  acre_hasInfantryPhone = 0;
+
+  class AcreRacks {
+   class Rack_1 {
+    name = "Vehicle Radio";
+    componentname = "ACRE_VRC111";
+    allowed[] = {};
+    disabled[] = {};
+    defaultComponents[] = {};
+    mountedRadio = "ACRE_PRC148";
+    isRadioRemovable = 0;
+    intercom[] = {};
+   };
+  };
+ };
+
+    //Trucks
+ class Truck_F;
+ class LIB_Truck_base : Truck_F {
+
+  // No Crew Intercom
+  acre_hasCrewIntercom = 0;
+
+  // No Passenger intercom
+  acre_hasPassengerIntercom = 0;
+
+  // No Infantry Phone
+  acre_hasInfantryPhone = 0;
+
+  class AcreRacks {
+   class Rack_1 {
+    name = "Vehicle Radio";
+    componentname = "ACRE_VRC111";
+    allowed[] = {};
+    disabled[] = {};
+    defaultComponents[] = {};
+    mountedRadio = "ACRE_PRC148";
+    isRadioRemovable = 0;
+    intercom[] = {};
+   };
+  };
+ };
+
+    //Tanks
+ class Tank_F;
+ class LIB_Tank_base : Tank_F {
+
+  // Crew Intercom
+  acre_hasCrewIntercom = 1;
+  acre_crewIntercomPositions[] = {"crew", "driver", "commander"};
+
+  // No Passenger intercom
+  acre_hasPassengerIntercom = 0;
+
+  // Infantry Phone
+  acre_hasInfantryPhone = 1;
+  acre_infantryPhoneIntercom[] = {"crew", "driver", "commander"};
+  acre_infantryPhonePosition[] = {0, 0, 0};
+
+  class AcreRacks {
+   class Rack_1 {
+    name = "Tank Radio";
+    componentname = "ACRE_VRC111";
+    allowed[] = {"commander", {"turret", "all"}};
+    disabled[] = {"gunner"};
+    defaultComponents[] = {};
+    mountedRadio = "ACRE_PRC148";
+    isRadioRemovable = 0;
+    intercom[] = {"crew", "driver", "commander"};
+   };
+  };
+ };
+
+    //Halftracks, usually
+ class LIB_WheeledTracked_APC_base : Tank_F {
+
+  // No Crew Intercom
+  acre_hasCrewIntercom = 0;
+
+  // No Passenger intercom
+  acre_hasPassengerIntercom = 0;
+
+  // No Infantry Phone
+  acre_hasInfantryPhone = 0;
+
+  class AcreRacks {
+   class Rack_1 {
+    name = "Vehicle Radio";
+    componentname = "ACRE_VRC111";
+    allowed[] = {};
+    disabled[] = {};
+    defaultComponents[] = {};
+    mountedRadio = "ACRE_PRC148";
+    isRadioRemovable = 0;
+    intercom[] = {};
+   };
+  };
+ };
+
+ class LIB_Halftrack_base : LIB_WheeledTracked_APC_base {};
+
+ //Inclusions
 #include "LIB_Boats.h"
 
 #include "LIB_Planes.h"
@@ -28,3 +191,27 @@ class cfgVehicles {
 #include "LIB_WheeledAPC.h"
 
 }; //End cfgVehicles
+
+/*class cfgSounds {
+ class InFoneGer {
+  name = "InFoneGer";
+  sound[] = {\IFA_ACRE2_COMPAT\Sounds\InFoneGer.wav};
+ };
+
+ class InFoneRus {
+  name = "InFoneRus";
+  sound[] = {\IFA_ACRE2_COMPAT\Sounds\InFoneRus.wav};
+ };
+
+ class InFoneUK {
+  name = "InFoneUK";
+  sound[] = {\IFA_ACRE2_COMPAT\Sounds\InFoneUK.wav};
+ };
+
+ class InFoneUS {
+  name = "InFoneUS";
+  sound[] = {\IFA_ACRE2_COMPAT\Sounds\InFoneUS.wav};
+ };
+
+}; //End cfgSound
+*/
